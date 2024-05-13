fef28ff0c66f1b7347e2326881ef5748c40860dc
lorenzoGrivet <lori.grivet@gmail.com>
2024-05-13 10:08:35 +0200

    sol

--- UI/controller.py
+++ UI/controller.py
@@ -9,8 +9,31 @@ class Controller:
         self._model = model
 
     def handleAnalizzaOggetti(self, e):
+        self._model.creaGrafo()
+        self._view._txt_result.controls.append(ft.Text(f"Grafo creato"))
+        self._view._txt_result.controls.append(ft.Text(f"Il grafo contiene {self._model.getNumNodes()} nodi"))
+        self._view._txt_result.controls.append(ft.Text(f"Il grafo contiene {self._model.getNumEdges()} archi"))
+        self._view.update_page()
         pass
 
     def handleCompConnessa(self,e):
+
+        idAdded = self._view._txtIdOggetto.value
+
+        try:
+            intId=int(idAdded)
+
+        except ValueError:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("Inserisci numero"))
+
+        if self._model.checkExistence(intId):
+            self._view._txt_result.controls.append(ft.Text(f"L'oggetto {intId} è presente nel grafo"))
+        else:
+            self._view._txt_result.controls.append(ft.Text(f"L'oggetto {intId} non è presente nel grafo"))
+
+        self._view.update_page()
+
+
         pass
 
--- model/model.py
+++ model/model.py
@@ -1,3 +1,44 @@
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._artObjectList=DAO.getAllObjects()
+        self._grafo=nx.Graph()
+        self._grafo.add_nodes_from(self._artObjectList)
+        self._idMap={}
+        for v in self._artObjectList:
+            self._idMap[v.object_id]=v
+
+
+    def creaGrafo(self):
+        self.addEdges()
+
+
+    def addEdges(self):
+        self._grafo.clear_edges()
+
+        #ciclo sui nodi se grafo è piccolo
+        # for u in self._artObjectList:
+        #     for v in self._artObjectList:
+        #         peso = DAO.getPeso(u,v)
+        #         self._grafo.add_edge(u,v,weigth=peso)
+
+        #oppure faccio query
+        allEdges=DAO.getAllConnessioni(self._idMap)
+
+        for e in allEdges:
+            self._grafo.add_edge(e.v1,e.v2,weigth=e.peso)
+
+
+
+    def checkExistence(self,idOggetto):
+        return idOggetto in self._idMap
+
+    def getNumNodes(self):
+        return len(self._grafo.nodes)
+
+    def getNumEdges(self):
+        return len(self._grafo.edges)
